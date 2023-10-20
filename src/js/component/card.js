b098a66704699b99b6df7b8e6fbd559a7bcaea11
IvanStacy <139801928+IvanStacy@users.noreply.github.com>
2023-10-20 14:45:00 +0000

    image

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,13 +3,44 @@ import { Link } from "react-router-dom";
 
 export const Card = (props) => {
   let item = props.item;
+  let imageBaseUrl;
+  let fallbackImgLink;
+
+  switch (props.category) {
+    case "characters":
+      imageBaseUrl =
+        "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/";
+      break;
+    case "planets":
+      imageBaseUrl =
+        "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/planets/";
+      fallbackImgLink = "https://www.horizonplant.com/wp-content/uploads/2017/05/placeholder-400x400.png";
+      break;
+    default:
+      imageBaseUrl =
+        "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/vehicles/";
+      fallbackImgLink = "https://marvel-b1-cdn.bc0a.com/f00000000279829/uwf.edu/media/university-of-west-florida/t4test/jason/images/600x400-1.png";
+      break;
+  }
+
+  const imgLink = `${imageBaseUrl}${parseInt(props.index) + 1}.jpg`;
+
+  const handleImageError = (event) => {
+    event.target.src = fallbackImgLink;
+  };
+
+
   return (
     <div className="card" style={{ width: "17rem" }}>
-      <img src={
+      <img
+        src={imgLink}
+        onError={handleImageError}
+      />
+      {/* <img src={
         props.category=="characters"?"https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/"+(props.index+1)+".jpg": 
         props.category=="planets"?"https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/planets/"+(props.index+1)+".jpg":
         "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/vehicles/"+(props.index+1)+".jpg"
-      } className="card-img-top" alt="..." />
+      } className="card-img-top" alt="..." /> */}
       <div className="card-body">
         <h5 className="card-title ">{item.name}</h5>
         <p className="card-text">
--- src/js/views/Details.js
+++ src/js/views/Details.js
@@ -11,42 +11,48 @@ export const Details = (props) => {
   let item = store[category][index];
 
   let imageBaseUrl;
-  
-  switch(category) {
-    case "characters": 
-      imageBaseUrl = "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/";
+
+  switch (category) {
+    case "characters":
+      imageBaseUrl =
+        "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/";
       break;
     case "planets":
-      imageBaseUrl = "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/planets/"
+      imageBaseUrl =
+        "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/planets/";
       break;
     default:
-      imageBaseUrl =  "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/vehicles/";
+      imageBaseUrl =
+        "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/vehicles/";
       break;
   }
 
   const imgLink = `${imageBaseUrl}${parseInt(index) + 1}.jpg`;
+  const fallbackImgLink = "https://www.horizonplant.com/wp-content/uploads/2017/05/placeholder-400x400.png"; // Replace with your actual fallback image URL
+
+  const handleImageError = (event) => {
+    event.target.src = fallbackImgLink;
+  };
 
   return (
     <div className="container">
       <div className="top d-flex">
         <div>
-        {/* <img height={550} width={500} src={
-        category==="characters"?"https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/"+(index+1)+".jpg": 
-        category=="planets"?"https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/planets/"+(index+1)+".jpg":
-        "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/vehicles/"+(index+1)+".jpg"
-      } />  */}
-
-      <img height={500} src={imgLink} />
+          <img
+            height={500}
+            src={imgLink}
+            onError={handleImageError}
+          />
         </div>
         <div>
-          <h3 className="ps-3" >{item.name}</h3>
+          <h3 className="ps-3">{item.name}</h3>
           <p className="p-5 text-center">
-          Vi jobbet sammen i samme redaksjon, men skjærte oss ikke direkte og kommuniserte bare av og til.
-          Jeg skrev nyhetene, og Misha og gutta hans valgte ut illustrasjoner for dem.
-          Jeg ble venn med ham på Facebook da han arrangerte en videosending fra et firmaarrangement, 
-          så jeg og andre eksterne ansatte deltok også i det. To uker senere tok han ferie og fløy til meg. 
-          Det var 12. juni 2016, den dagen Misha fridde til meg. Han sa: "Jeg har allerede levd et langt liv,
-          jeg har sett og opplevd mye, nå vil jeg vie meg til å gjøre deg lykkelig." Og han begynte å gjøre meg glad hver dag.
+            Vi jobbet sammen i samme redaksjon, men skjærte oss ikke direkte og kommuniserte bare av og til.
+            Jeg skrev nyhetene, og Misha og gutta hans valgte ut illustrasjoner for dem.
+            Jeg ble venn med ham på Facebook da han arrangerte en videosending fra et firmaarrangement,
+            så jeg og andre eksterne ansatte deltok også i det. To uker senere tok han ferie og fløy til meg.
+            Det var 12. juni 2016, den dagen Misha fridde til meg. Han sa: "Jeg har allerede levd et langt liv,
+            jeg har sett og opplevd mye, nå vil jeg vie meg til å gjøre deg lykkelig." Og han begynte å gjøre meg glad hver dag.
           </p>
         </div>
       </div>
@@ -57,26 +63,35 @@ export const Details = (props) => {
           {item.name}
         </div>
         <div className="col">
-          <h7><strong>{category=="characters"?"Birth Year": category=="planets"?"Climate":"Manufacturer"}</strong></h7>
-          <p>{item.birth_year||item.climate||item.manufacturer}</p>
+          <h7><strong>{category === "characters" ? "Birth Year" : category === "planets" ? "Climate" : "Manufacturer"}</strong></h7>
+          <p>{item.birth_year || item.climate || item.manufacturer}</p>
         </div>
         <div className="col">
-          <h7><strong>{category=="characters"?"Gender": category=="planets"?"Population":"Length"}</strong></h7>
-          <p>{item.gender||item.population||item.length}</p>
+          <h7><strong>{category === "characters" ? "Gender" : category === "planets" ? "Population" : "Length"}</strong></h7>
+          <p>{item.gender || item.population || item.length}</p>
         </div>
         <div className="col">
-          <h7><strong>{category=="characters"?"Height": category=="planets"?"Orbital Period":"Model"}</strong></h7>
-          <p>{item.height||item.orbital_period||item.model}</p>
+          <h7><strong>{category === "characters" ? "Height" : category === "planets" ? "Orbital Period" : "Model"}</strong></h7>
+          <p>{item.height || item.orbital_period || item.model}</p>
         </div>
         <div className="col">
-          <h7><strong>{category=="characters"?"Skin Color": category=="planets"?"Rotation Period":"Created"}</strong></h7>
-          <p>{item.skin_color||item.rotation_period||item.created}</p>
+          <h7><strong>{category === "characters" ? "Skin Color" : category === "planets" ? "Rotation Period" : "Created"}</strong></h7>
+          <p>{item.skin_color || item.rotation_period || item.created}</p>
         </div>
         <div className="col">
-          <h7><strong>{category=="characters"?"Eye Color": category=="planets"?"Diameter":"Max Atmosphering Speed"}</strong></h7>
-          <p>{item.eye_color||item.diameter||item.max_atmosphering_speed}</p>
+          <h7><strong>{category === "characters" ? "Eye Color" : category === "planets" ? "Diameter" : "Max Atmosphering Speed"}</strong></h7>
+          <p>{item.eye_color || item.diameter || item.max_atmosphering_speed}</p>
         </div>
       </div>
     </div>
   );
 };
+
+
+
+
+
+
+
+
+
